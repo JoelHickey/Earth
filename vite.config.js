1b30579b41638d721b0132f16fba4b443f1c3d73
Your Name <your-email@example.com>
2025-10-19 11:43:53 +1000

    Fix vite base path to only apply in production

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Earth/',
+  base: process.env.NODE_ENV === 'production' ? '/Earth/' : '/',
   server: {
     port: 3000,
     open: true
